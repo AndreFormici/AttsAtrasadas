48470808bc47c296ae2feeedde203dc1e8c32997
AndreFormici <andre.formici@users.noreply.github.com>
2020-02-19 14:49:35 -0300

    Atts do Pudim
    Ta dificil mandar essa bagaça

---
+++ confirmacao.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirmação</title>
+</head>
+<body>
+    <h1>Parabéns, seus dados foram cadastrados com sucesso<h1>
+    <h2>Caso queira entrar em contato com nossos administradores essas são as formas:</h2>
+    <p>E-mail: exemplodeemail@provedor.com</p>
+    <p>Telefone: (16)9900-0099</p>
+    <p>Celular: (16)90909-0909</p>
+</body>
+</html>
---
+++ formulario.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <h1>Preencha com seus dados</h1>
+    <div class="form-group text-left">
+        <label for="">Email</label>
+        <input type="text" class="form-control" name="email" id="email">
+    </div>
+    <div class="form-group text-left">
+        <label for="">Nome</label>
+        <input type="text" class="form-control" name="nome" id="nome">
+    </div>
+    <div class="form-group text-left">
+        <label for="">Senha</label>
+        <input type="text" class="form-control" name="senha" id="senha">
+    </div>
+    <div class="form-group text-left">
+        <label for="">Confirme sua senha</label>
+        <input type="text" class="form-control" name="confirma" id="confirma">
+    </div>
+    <div>
+        <button type="button" class="btn" href="confirmacao.php">Cadastrar</button>
+    </div>
+</body>
+</html>
